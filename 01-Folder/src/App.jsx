0e87d8a298ea6b3b8218343dca0ce31628ced6b7
Dev Patel <devp44239@gmail.com>
2026-07-26 15:46:01 +0530

    Learned chapter 1

---
+++ 01-Folder/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div id='parent'>
+      <h1 id='child1'>Hello world</h1>
+      <h2 id='child2'>Hello guys</h2>
+    </div>
+    <div id='chacha'>
+      <h1>Hello Chacha</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
