26cbb26988d2e78035fb51df1109ecd74d16015a
Dev Patel <devp44239@gmail.com>
2026-07-30 00:00:46 +0530

    Learn about Module CSS

---
+++ 05-CSS/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Button from './Component/Button/Button'
+import Header from './Component/Header/Header'
+
+const App = () => {
+  return (
+    <div>
+      <Button/>
+      <Header/>
+    </div>
+  )
+}
+
+export default App
